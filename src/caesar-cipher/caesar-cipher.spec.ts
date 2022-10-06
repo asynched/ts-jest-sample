f355bc2fb439224fe61cd510fde5629bd584a1b2
Matheus Alves Pereira <matttalves@gmail.com>
2022-10-05 22:08:48 -0300

    feat: Write tests by Matheus

---
+++ src/caesar-cipher/caesar-cipher.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Matheus Alves Pereira <matttalves@gmail.com>
+ */
+import { encrypt } from "./caesar-cipher";
+
+describe("caesar-cipher.ts", () => {
+  describe("encrypt", () => {
+    it("should encrypt string", () => {
+      expect(encrypt("hello", 5)).toBe("mjqqt");
+    });
+
+    it("should returns same string if shift value equals 0", () => {
+      expect(encrypt("hello", 0)).toBe("hello");
+    });
+
+    it.only("should encrypt uppercase letters", () => {
+      expect(encrypt("BraZil", 5)).toBe("Gwf_nq");
+    });
+
+    it("should encrypt uppercase letters", () => {
+      expect(encrypt("Banana is Delicious", 5)).toBe("Gfsfsf%nx%Ijqnhntzx");
+    });
+  });
+});
